c6136f7ed6084a6e76be3a3c70656cc1807d555b
Krishiv Manyam <krishivmanyam@gmail.com>
2026-03-02 13:26:20 -0800

    Initial Commit

---
+++ Anchor/AnchorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnchorApp.swift
+//  Anchor
+//
+//  Created by Krishiv Manyam on 3/2/26.
+//
+
+import SwiftUI
+
+@main
+struct AnchorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
